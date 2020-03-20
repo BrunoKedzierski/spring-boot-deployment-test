3b8007948801f571af916887289e392032b76efc
Bruno <bruno.kedzierski@wp.pl>
2020-03-20 17:45:28 +0100

    intit

---
+++ src/main/java/com/bkk/jokes_generator/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.bkk.jokes_generator.services;
+
+public interface JokeService {
+    String getJoke();
+}
---
+++ src/main/java/com/bkk/jokes_generator/services/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bkk.jokes_generator.services;
+
+import com.bkk.jokes_generator.services.JokeService;
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes quotes;
+
+    public JokeServiceImpl() {
+        this.quotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return quotes.getRandomQuote();
+    }
+}
